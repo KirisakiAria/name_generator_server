bd856d8fcfabb9359b44d7636c4c05be10beb9b6
kirisakiaria <xiaoli350791904@hotmail.com>
2020-07-09 17:41:56 +0800

    添加密钥、验证方法

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const Koa = require('koa')
 const config = require('./config/config')
 const static = require('koa-static')
 const api = require('./api')
+const JWT = require('./utils/jwt')
+
 const app = new Koa()
+
+// const jwt = new JWT(ctx.request.header.authorization)
+// const res = jwt.verifyToken()
 
 app.use(static(path.join(__dirname) + '/public/'))
 
---
+++ utils/JWT.js
@@ -0,0 +1,62 @@
+const fs = require('fs')
+const path = require('path')
+const jwt = require('jsonwebtoken')
+
+class JWT {
+  //生成时初始化传username，校验时初始化传token
+  constructor(data) {
+    this.data = data
+  }
+
+  //生成
+  generateToken() {
+    let cert = fs.readFileSync(
+      path.resolve(__dirname, '../pem/pkcs8_rsa_private_key.pem'),
+    ) //私钥 可以自己生成
+    let token = jwt.sign(
+      {
+        username: this.data.username,
+        roleId: this.data.roleId,
+      },
+      cert,
+      {
+        header: {
+          typ: 'JWT',
+          alg: 'RS256',
+        },
+        algorithm: 'RS256',
+        //expiresIn: '15 days',
+        issuer: 'kirisakiaria',
+        audience: 'kirisakiaria',
+      },
+    )
+    return token
+  }
+
+  //验证，返回解包的username
+  verifyToken() {
+    let token = this.data
+    let cert = fs.readFileSync(
+      path.join(__dirname, '../pem/rsa_public_key.pem'),
+    ) //公钥 可以自己生成
+    let res
+    try {
+      let result = jwt.verify(token, cert, { algorithms: ['RS256'] })
+      if (result) {
+        res = {
+          code: '1000',
+          username: result.username,
+          roleId: result.roleId,
+        }
+      }
+    } catch (e) {
+      res = {
+        message: '验证token出错',
+      }
+    }
+    return res
+  }
+}
+
+//生成token
+module.exports = JWT
