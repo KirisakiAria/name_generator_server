7e4819ff5cbb5c87b064d315c7d4fc4f146304e9
kirisakiaria <xiaoli350791904@hotmail.com>
2020-08-10 23:41:10 +0800

    开发服务模块

--- api/service.js
+++ api/service.js
@@ -4,7 +4,7 @@ const { verifyAdminLogin } = require('../utils/verify')
 
 const router = new Router({ prefix: '/service' })
 
-router.get('/', async ctx => {
+router.get('/', verifyAdminLogin, async ctx => {
   try {
     const data = await ServiceModel.find()
     ctx.body = {
@@ -21,13 +21,74 @@ router.get('/', async ctx => {
   }
 })
 
-router.post('/privacypolicy', verifyAdminLogin, async ctx => {
+router.get('/privacypolicy', async ctx => {
   try {
     const data = await ServiceModel.find()
     ctx.body = {
       code: '1000',
       message: '请求成功',
-      data: data[0],
+      data: data[0].privacyPolicy,
+    }
+  } catch (e) {
+    console.log(e)
+    ctx.body = {
+      code: '9000',
+      message: '请求错误',
+    }
+  }
+})
+
+router.get('/terms', async ctx => {
+  try {
+    const data = await ServiceModel.find()
+    ctx.body = {
+      code: '1000',
+      message: '请求成功',
+      data: data[0].terms,
+    }
+  } catch (e) {
+    console.log(e)
+    ctx.body = {
+      code: '9000',
+      message: '请求错误',
+    }
+  }
+})
+
+router.get('/usage', async ctx => {
+  try {
+    const data = await ServiceModel.find()
+    ctx.body = {
+      code: '1000',
+      message: '请求成功',
+      data: data[0].usage,
+    }
+  } catch (e) {
+    console.log(e)
+    ctx.body = {
+      code: '9000',
+      message: '请求错误',
+    }
+  }
+})
+
+router.put('/:id', verifyAdminLogin, async ctx => {
+  try {
+    const { privacyPolicy, terms, usage } = ctx.request.body
+    const result = await ServiceModel.updateOne(
+      { _id: ctx.params.id },
+      { $set: { privacyPolicy, terms, usage } },
+    )
+    if (result.ok == 1 && result.nModified == 1) {
+      ctx.body = {
+        code: '1000',
+        message: '保存成功',
+      }
+    } else {
+      ctx.body = {
+        code: '2000',
+        message: '保存失败',
+      }
     }
   } catch (e) {
     console.log(e)
