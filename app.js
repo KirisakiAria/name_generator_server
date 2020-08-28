d01d0334eba4dd40411deeb9e6d31a34086fd476
kirisakiaria <xiaoli350791904@hotmail.com>
2020-08-28 11:00:30 +0800

    添加集群功能

--- app.js
+++ app.js
@@ -1,24 +1,12 @@
 const path = require('path')
 const Koa = require('koa')
-const mongoose = require('mongoose')
+
 const koaBody = require('koa-body')
 const static = require('koa-static')
 const bodyParser = require('koa-bodyparser')
 const cors = require('koa2-cors')
 const api = require('./api')
-const config = require('./config/config')
 const views = require('koa-views')
-
-mongoose
-  .connect(config.db, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(
-    () => {
-      console.log('数据库连接成功，端口为27017')
-    },
-    err => {
-      console.log(`数据库连接失败！错误：${err}`.red)
-    },
-  )
 
 const app = new Koa()
 app.use(
