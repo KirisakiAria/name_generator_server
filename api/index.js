88d9e1305827b4f11b5707678c6cdf3523862134
kirisakiaria <xiaoli350791904@hotmail.com>
2020-09-17 21:48:13 +0800

    fix: 修复一些问题

--- api/index.js
+++ api/index.js
@@ -8,25 +8,21 @@ const word = require('./word')
 const application = require('./application')
 const service = require('./service')
 const information = require('./information')
-const web = require('./web')
 const dictionary = require('./dictionary')
 const system = require('./system')
 
-const apiRouter = new Router({ prefix: `/api/${config.apiVersion}` })
+const api = new Router({ prefix: `/api/${config.apiVersion}` })
 
 //验证
-apiRouter.use(admin.routes()).use(admin.allowedMethods())
-apiRouter.use(user.routes()).use(user.allowedMethods())
-apiRouter.use(sendcode.routes()).use(sendcode.allowedMethods())
-apiRouter.use(upload.routes()).use(upload.allowedMethods())
-apiRouter.use(word.routes()).use(word.allowedMethods())
-apiRouter.use(application.routes()).use(application.allowedMethods())
-apiRouter.use(service.routes()).use(service.allowedMethods())
-apiRouter.use(information.routes()).use(information.allowedMethods())
-apiRouter.use(dictionary.routes()).use(dictionary.allowedMethods())
-apiRouter.use(system.routes()).use(system.allowedMethods())
+api.use(admin.routes()).use(admin.allowedMethods())
+api.use(user.routes()).use(user.allowedMethods())
+api.use(sendcode.routes()).use(sendcode.allowedMethods())
+api.use(upload.routes()).use(upload.allowedMethods())
+api.use(word.routes()).use(word.allowedMethods())
+api.use(application.routes()).use(application.allowedMethods())
+api.use(service.routes()).use(service.allowedMethods())
+api.use(information.routes()).use(information.allowedMethods())
+api.use(dictionary.routes()).use(dictionary.allowedMethods())
+api.use(system.routes()).use(system.allowedMethods())
 
-const webRouter = new Router()
-webRouter.use(web.routes()).use(web.allowedMethods())
-
-module.exports = { apiRouter, webRouter }
+module.exports = api
--- app.js
+++ app.js
@@ -1,24 +1,16 @@
 const path = require('path')
 const Koa = require('koa')
-
 const koaBody = require('koa-body')
 const static = require('koa-static')
 const bodyParser = require('koa-bodyparser')
 const cors = require('koa2-cors')
 const api = require('./api')
-const views = require('koa-views')
 
 const app = new Koa()
-app.use(
-  views(path.join(__dirname, './static'), {
-    extension: 'ejs',
-  }),
-)
 app.use(cors())
 app.use(koaBody())
 app.use(static(path.join(__dirname) + '/public/'))
 app.use(bodyParser())
-app.use(api.apiRouter.routes())
-app.use(api.webRouter.routes())
+app.use(api.routes())
 
 module.exports = app
