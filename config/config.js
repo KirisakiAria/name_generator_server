222c02a6384020309a4de92a71053a299d33445e
kirisakiaria <xiaoli350791904@hotmail.com>
2020-12-17 17:30:01 +0800

    feat: 修复一些问题

--- config/config.js
+++ config/config.js
@@ -1,7 +1,8 @@
 module.exports = {
   // db:
   //   'mongodb://kirisakiaria:PbVAFRa1DcdkIa2y@localhost:27017/name_generator?authSource=admin?authMechanism=SCRAM-SHA-256',
-  db: 'mongodb://kirisakiaria:ihSvydZrQQW9RSud@localhost:27017/name_generator',
+  db:
+    'mongodb://kirisakiaria:ihSvydZrQQW9RSud@localhost:27017/name_generator?authSource=admin?authMechanism=SCRAM-SHA-256',
   port: 2077,
   apiVersion: 'v1',
   alipayAppId: '2021002115614424',
