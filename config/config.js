ee79e5d8c60d73ce150e7a1b7f7f010263979752
kirisakiaria <xiaoli350791904@hotmail.com>
2020-12-18 18:25:04 +0800

    feat: 删除配置项

--- config/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  db:
-    'mongodb://kirisakiaria:PbVAFRa1DcdkIa2y@localhost:27017/name_generator?authSource=admin?authMechanism=SCRAM-SHA-256',
-  // db:
-  //   'mongodb://kirisakiaria:ihSvydZrQQW9RSud@localhost:27017/name_generator',
-  port: 2077,
-  apiVersion: 'v1',
-  alipayAppId: '2021002115614424',
-}
