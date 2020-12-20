62616d95ae3c56c63b1102d6e7797cfb0687ebf8
kirisakiaria <xiaoli350791904@hotmail.com>
2020-12-20 19:41:31 +0800

    feat: 去除限制花呗分期

--- api/user.js
+++ api/user.js
@@ -715,10 +715,6 @@ router.post('/pay', verifyAppBaseInfo, verifyUserLogin, async ctx => {
             productCode: 'QUICK_MSECURITY_PAY',
             enablePayChannels:
               'balance,moneyFund,bankPay,debitCardExpress,creditCard,credit_group,coupon,',
-            extendParams: {
-              hbFqNum: '3',
-              hbFqSellerPercent: '0',
-            },
           })
           /** 异步通知地址，商户外网可以post访问的异步地址，用于接收支付宝返回的支付结果，如果未收到该通知可参考该文档进行确认：https://opensupport.alipay.com/support/helpcenter/193/201602475759 **/
           //formData.addField('notifyUrl', 'https://www.baidu.com')
